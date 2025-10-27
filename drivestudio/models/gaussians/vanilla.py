8690d68bd6c46a4e1e496b87a0e02cf4d61fd60a
HugoChass <hugochassagnette15@gmail.com>
2025-10-28 00:15:17 +0100

    removed old print

--- drivestudio/models/gaussians/vanilla.py
+++ drivestudio/models/gaussians/vanilla.py
@@ -265,7 +265,6 @@ class VanillaGaussians(nn.Module):
                 self._opacities = Parameter(torch.cat([self._opacities.detach(), split_opacities, dup_opacities], dim=0))
                 self._scales = Parameter(torch.cat([self._scales.detach(), split_scales, dup_scales], dim=0))
                 self._quats = Parameter(torch.cat([self._quats.detach(), split_quats, dup_quats], dim=0))
-                print(self._semantics.shape, self._semantics.detach().shape, split_semantics.shape, dup_semantics.shape)
                 self._semantics = Parameter(torch.cat([self._semantics.detach(), split_semantics, dup_semantics], dim=0)) # NEW
                 
                 # append zeros to the max_2Dsize tensor
