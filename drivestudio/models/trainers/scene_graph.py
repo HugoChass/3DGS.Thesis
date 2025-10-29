3897cf454b6304bfcf7cb7037ab09d82185b8439
HugoChass <hugochassagnette15@gmail.com>
2025-10-29 02:31:42 +0100

    fix

--- drivestudio/models/trainers/scene_graph.py
+++ drivestudio/models/trainers/scene_graph.py
@@ -304,7 +304,7 @@ class MultiTrainer(BasicTrainer):
         palette = get_semantic_palette(device=device, dtype=dtype)
         gt_semantic = image_infos["lidar_semantics_map"]
         H, W = gt_semantic.shape
-        gt_semantic_map = torch.tensor(gt_semantic, device=device, dtype=dtype)
+        gt_semantic = torch.tensor(gt_semantic, device=device, dtype=torch.long)
         gt_semantic_map = palette[gt_semantic.view(-1)].view(H, W, 3).clone()
         outputs.update({"gt_semantic_map": gt_semantic_map})
         
