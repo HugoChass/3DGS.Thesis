542b620f96a5eb964510a1b235988ef6cf88049f
HugoChass <hugochassagnette15@gmail.com>
2025-12-21 14:49:55 +0100

    revert + prelim test

--- drivestudio/tools/train.py
+++ drivestudio/tools/train.py
@@ -255,6 +255,7 @@ def main(args):
                                  'per_class_recall':render_results['per_class_recall'],
                                  'per_class_f1':render_results['per_class_f1'],
                                  }
+            print(image_metric_dict)
 
             if args.enable_wandb:
                 wandb.log(
@@ -387,7 +388,7 @@ def main(args):
                 outputs=outputs,
                 image_infos=image_infos,
             )
-        metric_logger.update(**{"train_metrics/"+k: v for k, v in image_metric_dict.items()})
+        metric_logger.update(**{"train_metrics/"+k: v.item() for k, v in metric_dict.items()})
         metric_logger.update(**{"train_stats/gaussian_num_" + k: v for k, v in trainer.get_gaussian_count().items()})
         metric_logger.update(**{"losses/"+k: v.item() for k, v in loss_dict.items()})
         metric_logger.update(**{"train_stats/lr_" + group['name']: group['lr'] for group in trainer.optimizer.param_groups})
