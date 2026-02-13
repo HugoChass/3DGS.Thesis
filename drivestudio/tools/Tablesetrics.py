c956560862b71f24650e3790e59ebf7ad17c13c3
HugoChass <hugochassagnette15@gmail.com>
2026-02-13 03:47:28 +0100

    tables for v2

--- drivestudio/tools/Tablesetrics.py
+++ drivestudio/tools/Tablesetrics.py
@@ -8,7 +8,7 @@ import pandas as pd
 # ==========================
 # CONFIG
 # ==========================
-MAIN_FOLDER = "/tudelft.net/staff-umbrella/hchassagnette/Workspace/output/ThesisResults"
+MAIN_FOLDER = "/tudelft.net/staff-umbrella/hchassagnette/Workspace/output/ThesisResultsV2"
 OUT_DIR = os.path.join(MAIN_FOLDER, "tables")
 os.makedirs(OUT_DIR, exist_ok=True)
 
